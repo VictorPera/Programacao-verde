dcc00aef81e9ca05da2e204ae35bbd7e916c20d7
Victor Hugo Prado Pera <102368651+VictorPera@users.noreply.github.com>
2022-05-17 07:39:52 -0300

    Add files via upload
    não reclama Jão

---
+++ calculadora da aula/js/calculadora.js
@@ -0,0 +1,7 @@
+alert("Você está prestes a ver uma calculadora fantástica")//notificação de interação na página
+function mudartextoeacao()
+{
+    document.getElementById("botao").innerHTML = "viu?" //aqui estamos modificando o botão via Id
+    alert("ira explodir já já")
+    createImageBitmap
+}
