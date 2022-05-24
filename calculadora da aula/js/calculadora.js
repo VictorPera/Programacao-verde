85c8c982be71bad0092787cb0cf06131df8db69e
Victor Hugo Prado Pera <102368651+VictorPera@users.noreply.github.com>
2022-05-24 08:18:13 -0300

    Delete calculadora da aula/js directory

--- calculadora da aula/js/calculadora.js
+++
@@ -1,7 +0,0 @@
-alert("Você está prestes a ver uma calculadora fantástica")//notificação de interação na página
-function mudartextoeacao()
-{
-    document.getElementById("botao").innerHTML = "viu?" //aqui estamos modificando o botão via Id
-    alert("ira explodir já já")
-    createImageBitmap
-}
