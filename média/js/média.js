03b4731a5338531e7aa24a7a5e49ee2c3d0dd5fb
Victor Hugo Prado Pera <102368651+VictorPera@users.noreply.github.com>
2022-06-01 08:18:51 -0300

    Add files via upload

---
+++ média/js/média.js
@@ -0,0 +1,12 @@
+function media(){
+    var nota1 = parseFloat(document.getElementById("nota1").value);
+    var nota2 = parseFloat(document.getElementById("nota2").value);
+
+    var media = (nota1 + nota2 / 2);
+
+    if(media >= 7){
+        alert("Passou na média");
+    }else{
+        alert("Não passou");
+    }
+}
