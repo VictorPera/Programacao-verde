82e1a2edabfde7f5c943ffcd0034988e206c3e4c
Victor Hugo Prado Pera <102368651+VictorPera@users.noreply.github.com>
2022-06-01 09:52:54 -0300

    Add files via upload

---
+++ validação/senha.js
@@ -0,0 +1,11 @@
+function verificar(){
+    var senha = "a1";
+    senha = document.getElementById("valor").value;
+
+    if(senha == "a1"){
+        alert("Entrou");
+    }else{
+        alert("Digite novamente");
+    }
+}
+    
